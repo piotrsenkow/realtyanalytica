0bfea6a3c3778f1776f71d1ab9d1ca43138cde74
Piotr Senkow <piotrsenkow@Piotrs-MacBook-Pro.local>
2024-01-03 22:46:35 -0600

    Dynamic routing for propertydetails page

--- backend/graphql-service/server.js
+++ backend/graphql-service/server.js
@@ -1,13 +1,30 @@
+// server.js
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema'); // You will create this in the next steps
+const { postgraphile } = require('postgraphile');
+const cors = require('cors');
+
 
 const app = express();
+const PORT = process.env.PORT || 3002;
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true, // Enables the GraphiQL tool
+// PostgreSQL connection string
+const DATABASE_URL = 'postgresql://piotr:2050@localhost:5432/mred3?sslmode=disable';
+
+// Enable CORS for your Next.js app domain
+app.use(cors({
+  origin: 'http://localhost:3000', // Adjust this to match your Next.js app's domain
+  credentials: true
 }));
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// PostGraphile middleware
+app.use(
+  postgraphile(DATABASE_URL, 'public', {
+    watchPg: true,
+    graphiql: true,
+    enhanceGraphiql: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
